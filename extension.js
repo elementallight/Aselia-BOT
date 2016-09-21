79a729ffb3e61ebc292daadc5259d338f9959bca
blademan729 <dvandenberg97@gmail.com>
2016-09-21 14:52:35 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -67,6 +67,19 @@
           }
         };
         
+		  bot.commands.uptimeCommand = {
+          command: 'uptime',  // The command to be called. With the standard command literal this would be: !bacon
+          rank: 'user', // Minimum user permission to use the command
+          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+          functionality: function (chat, cmd) {
+            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+            if (!bot.commands.executable(this.rank, chat)) return void (0);
+            else {
+			var msg = 'Aselia bot has been up for ' + since;
+              API.sendChat(msg);
+            }
+          }
+        };
         // Load the chat package again to account for any changes
         bot.loadChat();
 
