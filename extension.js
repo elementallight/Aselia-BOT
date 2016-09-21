fd31f984c9b1760ca33edc904628de7a3f9bcfe4
blademan729 <dvandenberg97@gmail.com>
2016-09-21 15:34:50 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -53,6 +53,19 @@
           }
         };
         
+		bot.commands.noitmeseverCommand = {
+          command: 'NIE',  // The command to be called. With the standard command literal this would be: !bacon
+          rank: 'bouncer', // Minimum user permission to use the command
+          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+          functionality: function (chat, cmd) {
+            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+            if (!bot.commands.executable(this.rank, chat)) return void (0);
+            else {
+              API.sendChat("NO !!! ITEMS !!!! EVERRRRRRRRRRR!!!!!!");
+			  API.sendChat("https://i.imgur.com/IOdibFg.png");
+            }
+          }
+        };
         
         bot.commands.dhapsCommand = {
           command: 'dhaps',  // The command to be called. With the standard command literal this would be: !bacon
