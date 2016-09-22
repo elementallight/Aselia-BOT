6602967ce98c3ee952bf6a70b3e67a6b002ad66f
blademan729 <dvandenberg97@gmail.com>
2016-09-21 22:35:24 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -107,6 +107,19 @@
           }
         };
 		
+		bot.commands.emergancymaintanaceCommand = {
+          command: 'emmaint',  // The command to be called. With the standard command literal this would be: !bacon
+          rank: 'manager', // Minimum user permission to use the command
+          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+          functionality: function (chat, cmd) {
+            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+            if (!bot.commands.executable(this.rank, chat)) return void (0);
+            else {
+              API.sendChat("Aselia bot will go down for emergancy maintanace starting now.");
+            }
+          }
+        };
+		
         // Load the chat package again to account for any changes
         bot.loadChat();
 
@@ -160,7 +173,7 @@
       afkRankCheck: "ambassador",
       motdEnabled: true,
       motdInterval: 10,
-      motd: "Welcome to Aselia! Are you enjoying your stay?" + "why not add a song or two to Aselia Bots playlist which is updated every other week!" + "Playlist: http://bit.ly/2cIexgT",
+      motd: "Welcome to Aselia! Are you enjoying your stay?" + "why not add a song or two to Aselia Bots playlist which is updated every other week!" + "\n" + "Playlist: http://bit.ly/2cIexgT",
       filterChat: true,
       etaRestriction: false,
       welcome: true,
