d867181910355e335e8a775a358ee4feadcd466a
blademan729 <dvandenberg97@gmail.com>
2017-05-22 17:53:27 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 (function () {
 
     // Change this to your GitHub username so you don't have to modify so many things.
-    var fork = "blademan729";
+    var fork = "basicBot";
 
     // Define our function responsible for extending the bot.
     function extend() {
@@ -15,15 +15,6 @@
 
         // Load custom settings set below
         bot.retrieveSettings();
-        retrieveFromStorage();
-            window.bot = basicBot;
-            basicBot.roomUtilities.updateBlacklists();
-            setInterval(basicBot.roomUtilities.updateBlacklists, 60 * 60 * 1000);
-            basicBot.getNewBlacklistedSongs = basicBot.roomUtilities.exportNewBlacklistedSongs;
-            basicBot.logNewBlacklistedSongs = basicBot.roomUtilities.logNewBlacklistedSongs;
-            if (basicBot.room.roomstats.launchTime === null) {
-                basicBot.room.roomstats.launchTime = Date.now();
-            }
 
         //Extend the bot here, either by calling another function or here directly.
 
@@ -32,103 +23,21 @@
         for (var i = 0; i < spamWords.length; i++) {
           window.bot.chatUtilities.spam.push(spamWords[i]);
         }
-        
+
         // Example code for a bot command:
-        bot.commands.ramenCommand = {
-          command: 'ramen',  // The command to be called. With the standard command literal this would be: !bacon
+        bot.commands.baconCommand = {
+          command: 'bacon',  // The command to be called. With the standard command literal this would be: !bacon
           rank: 'user', // Minimum user permission to use the command
           type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
           functionality: function (chat, cmd) {
             if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
             if (!bot.commands.executable(this.rank, chat)) return void (0);
             else {
-		var from = chat.un;
-		var msg = '@' + from + ' ' + "You eat Raine's cooking...You dont survive.";
-              API.sendChat(msg);
+              API.sendChat("/me Bacon!!!");
             }
           }
         };
-        
-      bot.commands.infoCommand = {
-          command: 'info',  // The command to be called. With the standard command literal this would be: !bacon
-          rank: 'bouncer', // Minimum user permission to use the command
-          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-          functionality: function (chat, cmd) {
-            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-            if (!bot.commands.executable(this.rank, chat)) return void (0);
-            else {
-              API.sendChat("Are you enjoying your stay?");
-            }
-          }
-        };
-        
-		bot.commands.noitmeseverCommand = {
-          command: 'NBSE',  // The command to be called. With the standard command literal this would be: !bacon
-          rank: 'bouncer', // Minimum user permission to use the command
-          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-          functionality: function (chat, cmd) {
-            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-            if (!bot.commands.executable(this.rank, chat)) return void (0);
-            else {
-              API.sendChat("NO !!! BOT !!!! SPAMMMM !!!! EVERRRRRRRRRRR!!!!!!" + "https://i.imgur.com/IOdibFg.png");
 
-            }
-          }
-        };
-		
-		bot.commands.playlistCommand = {
-          command: 'playlist',  // The command to be called. With the standard command literal this would be: !bacon
-          rank: 'user', // Minimum user permission to use the command
-          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-          functionality: function (chat, cmd) {
-            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-            if (!bot.commands.executable(this.rank, chat)) return void (0);
-            else {
-              API.sendChat("Paylist link: http://bit.ly/2cIczxc"+ " " + "Please remember to read the rules of the playlist and reframe from reposting songs from your own playlists!");
-
-            }
-          }
-        };
-        
-        bot.commands.dhapsCommand = {
-          command: 'dhaps',  // The command to be called. With the standard command literal this would be: !bacon
-          rank: 'user', // Minimum user permission to use the command
-          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-          functionality: function (chat, cmd) {
-            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-            if (!bot.commands.executable(this.rank, chat)) return void (0);
-            else {
-              API.sendChat("http://i.imgur.com/QpJZy1P.png");
-            }
-          }
-        };
-		
-		bot.commands.maintanaceCommand = {
-          command: 'maint',  // The command to be called. With the standard command literal this would be: !bacon
-          rank: 'manager', // Minimum user permission to use the command
-          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-          functionality: function (chat, cmd) {
-            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-            if (!bot.commands.executable(this.rank, chat)) return void (0);
-            else {
-              API.sendChat("Aselia bot will go down for brief maintanace in the next 5 minutes.");
-            }
-          }
-        };
-		
-		bot.commands.emergancymaintanaceCommand = {
-          command: 'emmaint',  // The command to be called. With the standard command literal this would be: !bacon
-          rank: 'manager', // Minimum user permission to use the command
-          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-          functionality: function (chat, cmd) {
-            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-            if (!bot.commands.executable(this.rank, chat)) return void (0);
-            else {
-              API.sendChat("Aselia bot will go down for emergancy maintanace starting now.");
-            }
-          }
-        };
-		
         // Load the chat package again to account for any changes
         bot.loadChat();
 
@@ -143,14 +52,14 @@
       scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
-      startupVolume: 0, // 0-100
+      startupVolume: 50, // 0-100
       startupEmoji: false, // true or false
       autowoot: true,
       autoskip: false,
       smartSkip: true,
       cmdDeletion: true,
       maximumAfk: 120,
-      afkRemoval: false,
+      afkRemoval: true,
       maximumDc: 60,
       bouncerPlus: true,
       blacklistEnabled: true,
@@ -159,12 +68,12 @@
       maximumLocktime: 10,
       cycleGuard: true,
       maximumCycletime: 10,
-      voteSkip: true,
-      voteSkipLimit: 5,
+      voteSkip: false,
+      voteSkipLimit: 10,
       historySkip: false,
       timeGuard: true,
       maximumSongLength: 10,
-      autodisable: true,
+      autodisable: false,
       commandCooldown: 30,
       usercommandsEnabled: true,
       skipPosition: 3,
@@ -176,13 +85,12 @@
       ["sound", "The song you played had bad sound quality or no sound. "],
       ["nsfw", "The song you contained was NSFW (image or sound). "],
       ["unavailable", "The song you played was not available for some users. "]
-	  ["10min+", "The song you played exceded the time limit of 10 Minutes."]
       ],
       afkpositionCheck: 15,
       afkRankCheck: "ambassador",
-      motdEnabled: true,
-      motdInterval: 10,
-      motd: "Welcome to Aselia! Are you enjoying your stay?" + "why not add a song or two to Aselia Bots playlist which is updated every other week!" + "\n" + "Playlist: http://bit.ly/2cIexgT",
+      motdEnabled: false,
+      motdInterval: 5,
+      motd: "Temporary Message of the Day",
       filterChat: true,
       etaRestriction: false,
       welcome: true,
